223714b58259387a299f4babb7b710be47783a12
End <git@endoftime.works>
2026-06-13 16:08:28 -0700

    feat(import): add importer source surface

---
+++ mobile/test/import_sources_test.dart
@@ -0,0 +1,37 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:pluris_haven/data/import/import_sources.dart';
+
+void main() {
+  test('defines the initial importer surface', () {
+    expect(
+      ImportSource.values.map((source) => source.label),
+      containsAll([
+        'Simply Plural',
+        'PluralKit file',
+        'PluralKit live',
+        'Tupperbox',
+        'PluralSpace',
+        'Prism',
+      ]),
+    );
+
+    expect(ImportSource.prism.inputKinds, [ImportInputKind.encryptedFile]);
+    expect(
+      ImportSource.pluralKitLive.dedupeKeys,
+      containsAll(['PluralKit UUIDs', 'PluralKit short IDs']),
+    );
+    expect(ImportSource.pluralKitLive.jobSource, 'pluralkit_api');
+    expect(ImportSource.tupperbox.jobSource, 'tupperbox_file');
+  });
+
+  test('documents the PluralKit live import shape', () {
+    const shape = PluralKitLiveImportShape();
+
+    expect(shape.authHeaderName, 'Authorization');
+    expect(shape.systemEndpoint, '/systems/@me');
+    expect(shape.membersEndpoint, '/systems/@me/members');
+    expect(shape.groupsEndpoint, '/systems/@me/groups?with_members=true');
+    expect(shape.switchesEndpoint, '/systems/@me/switches?limit=100');
+    expect(shape.pageDelay, const Duration(milliseconds: 600));
+  });
+}
