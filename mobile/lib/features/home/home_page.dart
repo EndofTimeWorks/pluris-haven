724602625cd76143cc1c7fae7cad09f71664041d
End <git@endoftime.works>
2026-07-03 14:18:29 -0700

    feat(mobile): schedule local reminders

--- mobile/lib/features/home/home_page.dart
+++ mobile/lib/features/home/home_page.dart
@@ -4,6 +4,7 @@ import 'dart:io';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -209,7 +210,11 @@ class _HomePageState extends State<HomePage> {
           ],
         );
       case SpSection.reminders:
-        return RemindersPage(repository: widget.repository);
+        return RemindersPage(
+          repository: widget.repository,
+          onNotificationSettings: () =>
+              _selectSection(SpSection.notificationHistory),
+        );
       case SpSection.privacyBuckets:
         return LocalPrivacyPage(onSelect: _selectSection);
       case SpSection.tokens:
