aba84985a07001487e3c679f66ecdde9262f9b51
End <git@endoftime.works>
2026-07-25 15:31:48 -0700

    style(format): normalize web and documentation files

--- website/svelte.config.js
+++ website/svelte.config.js
@@ -3,25 +3,25 @@ import { relative, sep } from 'node:path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	compilerOptions: {
-		// defaults to rune mode for the project, except for `node_modules`. Can be removed in svelte 6.
-		runes: ({ filename }) => {
-			const relativePath = relative(import.meta.dirname, filename);
-			const pathSegments = relativePath.toLowerCase().split(sep);
-			const isExternalLibrary = pathSegments.includes('node_modules');
+  compilerOptions: {
+    // defaults to rune mode for the project, except for `node_modules`. Can be removed in svelte 6.
+    runes: ({ filename }) => {
+      const relativePath = relative(import.meta.dirname, filename);
+      const pathSegments = relativePath.toLowerCase().split(sep);
+      const isExternalLibrary = pathSegments.includes('node_modules');
 
-			return isExternalLibrary ? undefined : true;
-		}
-	},
-	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: true,
-			strict: true
-		})
-	}
+      return isExternalLibrary ? undefined : true;
+    },
+  },
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: undefined,
+      precompress: true,
+      strict: true,
+    }),
+  },
 };
 
 export default config;
