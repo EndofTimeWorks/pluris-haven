81314b8e0dd72c9cfe704939c512c452d00e4837
End <git@endoftime.works>
2026-08-12 21:33:33 -0700

    test(mobile): validate inherited US English catalogue

--- mobile/test/localization_contract_test.dart
+++ mobile/test/localization_contract_test.dart
@@ -4,11 +4,14 @@ import 'dart:io';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('English localisation catalogues expose the same messages', () {
+  test('US English catalogue contains intentional base-English overrides', () {
     final english = _messagesIn('lib/l10n/app_en.arb');
     final usEnglish = _messagesIn('lib/l10n/app_en_US.arb');
 
-    expect(usEnglish.keys, unorderedEquals(english.keys));
+    expect(usEnglish.keys, everyElement(isIn(english.keys)));
+    for (final entry in usEnglish.entries) {
+      expect(entry.value, isNot(english[entry.key]));
+    }
     expect(english.values, everyElement(isNotEmpty));
     expect(usEnglish.values, everyElement(isNotEmpty));
   });
